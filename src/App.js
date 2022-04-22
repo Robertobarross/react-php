1451a4aef8331e7b99077a110ae22249cdaa1b91
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-04-21 21:08:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import {useEffect,useState} from 'react';
+import axios from 'axios'; // Adicionando o axios
 import './App.css';
 
 function App() {
 
-  useEffect(()=>{
-    alert('Olá, vamos começar?');
+  const [posts,setPosts] = useState([]);
+
+  useEffect(()=>{ // Consumindo Api
+    axios.get('http://localhost/Projetos-PHP/Api-Teste-PHP/')
+    .then(function(api){
+      setPosts(api.data);
+    })
+
   },[])
 
-  return (
+  return ( // Exibindo os itens da Api
     <div className="App">
-      <h1>Projeto ReactPHP</h1>
+      {
+        posts.map(function(itens){
+          return(
+            <div className="conteudo">
+                <img src={itens.imagem}/>
+                <p>{itens.conteudo}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
